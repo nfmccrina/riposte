6d74e25dc8f65e675dce34463da0bb2c1f45fb9a
Nathan McCrina <salmacis88@gmail.com>
2014-05-24 14:20:09 -0400

    0.2: completed read metadata from file feature

--- consolegui.py
+++ consolegui.py
@@ -1,6 +1,7 @@
 import ripper
 import ripdata
 import ripsettings
+import configobj
 
 class ConsoleGui:
     def __init__(self):
@@ -63,10 +64,12 @@ class ConsoleGui:
                 print('A: edit artist name')
                 print('b: back to main menu')
                 print('d: edit disc number')
+                print('f: import album info from file')
                 print('g: edit genre')
                 print('i: edit individual track artists')
                 print('t: edit track titles')
                 print('T: edit individual track titles')
+                print('v: toggle various artists')
                 print('y: edit year')
             elif usr_input == 'a':
                 self.cd_info['album'] = raw_input('Enter album title: ')
@@ -75,12 +78,19 @@ class ConsoleGui:
                 for num in range(0, self.cd_info['totaltracks']):
                     if artist == '':
                         self.cd_info['trackartists'][num] = raw_input('Enter track {0} artist: '.format(int(num + 1)))
+                        self.cd_info['variousartists'] = True
                     else:
                         self.cd_info['trackartists'][num] = artist
             elif usr_input == 'd':
                 try:
                     self.cd_info['discnumber'] = int(raw_input('Enter disc #: '))
                 except ValueError:
+                    pass
+            elif usr_input == 'f':
+                try:
+                    infile = open(raw_input('Enter data filename: '))
+                    self.cd_info = self.read_info_file(infile)
+                except IOError:
                     pass
             elif usr_input == 'g':
                 self.cd_info['genre'] = raw_input('Enter genre: ')
@@ -89,6 +99,7 @@ class ConsoleGui:
                     tracknum = int(raw_input('Enter track number to edit: '))
                     try:
                         self.cd_info['trackartists'][tracknum - 1] = raw_input('Enter track {0} artist: '.format(tracknum))
+                        self.cd_info['variousartists'] = True
                     except IndexError:
                         print('Error: track {0} does not exist'.format(tracknum))
                 except ValueError:
@@ -106,8 +117,31 @@ class ConsoleGui:
                         print('Error: track {0} does not exist'.format(tracknum))
                 except ValueError:
                     print('Invalid track number')
+            elif usr_input == 'v':
+                self.cd_info['variousartists'] = not self.cd_info['variousartists']
             elif usr_input == 'y':
                 try:
                     self.cd_info['year'] = int(raw_input('Enter year: '))
                 except ValueError:
                     pass
+
+    def read_info_file(self, f):
+        data = configobj.ConfigObj(f)
+        rdata = ripdata.RipData()
+        rdata.initialize()
+
+        for k in data.keys():
+            if k in rdata:
+                rdata[k] = data[k]
+
+        if 'artist' in data:
+            for num in range(0, rdata['totaltracks']):
+                rdata['trackartists'][num] = data['artist']
+
+        while (len(rdata['tracknames']) < rdata['totaltracks']):
+            rdata['tracknames'].extend(' ')
+
+        while (len(rdata['trackartists']) < rdata['totaltracks']):
+            rdata['trackartists'].extend(' ')
+
+        return rdata
